e7ff0f6dc424d424d2d81bd022546992ec4e2924
Gabriel-Danielson <xuludanielson@gmail.com>
2025-04-11 08:43:18 -0400

    Add files via upload

---
+++ Midterm/Midterm/Customer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Midterm
+{
+
+    class Customer
+    {
+        //Fields
+        private int _customerNumber;
+        private int _pin;
+        private int _checkingsAccountNumber;
+        private int _savingsAccountNumber;
+
+        //Constructor
+        public Customer()
+        {
+            _customerNumber = 0;
+            _pin = 0;
+            _checkingsAccountNumber = 0;
+            _savingsAccountNumber = 0;
+        }
+
+        //Properties
+        public int Customer_Number
+        {
+            get { return _customerNumber; }
+            set { _customerNumber = value; }
+        }
+        public int PIN
+        {
+            get { return _pin; }
+            set { _pin = value; }
+        }
+        public int Checkings_Account_Number
+        {
+            get { return _checkingsAccountNumber ; }
+            set { _checkingsAccountNumber = value; }
+        }
+        public int Savings_Account_Number
+        {
+            get { return _savingsAccountNumber; }
+            set { _savingsAccountNumber = value; }
+        }
+    }
+}
---
+++ Midterm/Midterm/Savings.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Midterm
+{
+
+    internal class Savings : Customer
+    {
+        //Fields
+        private int _accountNumber;
+        private decimal _balance;
+
+        //Constructor
+        public Savings()
+        {
+            _accountNumber = 0;
+        }
+
+        //Properties
+        public int Account_Number
+        {
+            get { return _accountNumber; }
+            set { _accountNumber = value; }
+        }
+        public decimal Balance
+        {
+            get { return _balance; }
+            set { _balance = value; }
+        }
+    }
+}
